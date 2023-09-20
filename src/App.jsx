1cc6c73c7250b6cb22dff8828f69640955ef36e8
Rohitbels <rohit.belsare@avoma.com>
2023-09-20 18:57:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Disperse from './Disperse'
+
+function App() {
+
+
+  return (
+    <>
+      <Disperse />
+    </>
+  )
+}
+
+export default App
